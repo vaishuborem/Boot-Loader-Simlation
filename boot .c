45da3c8d556c5858324940306b56add497948bce
Borem vaishnavi <135442689+vaishuborem@users.noreply.github.com>
2025-04-19 15:31:52 +0530

    Create boot .c

---
+++ boot .c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+#include <unistd.h>  
+void bios() { 
+printf("BIOS: Initializing hardware...\n"); 
+sleep(1); 
+printf("BIOS: POST (Power-On Self Test) completed.\n\n"); 
+} 
+void loadBootloader() { 
+printf("Bootloader: Loading bootloader into memory...\n"); 
+sleep(1); 
+printf("Bootloader: Bootloader ready.\n\n"); 
+} 
+void loadKernel() { 
+printf("Bootloader: Loading OS Kernel...\n"); 
+for(int i = 0; i <= 100; i += 25) { 
+printf("   Loading... %d%%\n", i); 
+sleep(1); 
+} 
+BOOT LOADER SIMULATION      
+OPERATING SYSTEMS 
+printf("Bootloader: Kernel successfully loaded.\n\n"); 
+} 
+void startOS() { 
+printf("Kernel: Starting the Operating System...\n"); 
+sleep(1); 
+printf("OS is now running! Welcome!\n"); 
+} 
+int main() { 
+printf("Power ON\n\n"); 
+bios(); 
+loadBootloader(); 
+loadKernel(); 
+startOS(); 
+return 0; 
+}
